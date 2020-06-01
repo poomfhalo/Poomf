01b05e7e4443db0a645ce1bb599fffb6d72b5a65
Motasem Zakarneh <mutrz99@gmail.com>
2020-06-01 18:56:43 +0300

    Created, Preperations, to start working on multiplayer

---
+++ Assets/00_DodgeBall/ScriptsMP/LaunchManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+using Photon.Pun;
+
+public class LaunchManager : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        print("Connected To Master");
+    }
+}
