8e1548c1121f03c21cb73f93d9c19deeb76ef4b3
Motasem Zakarneh <mutrz99@gmail.com>
2020-06-02 16:29:02 +0300

    Completed Preperations, for structure for having multiple characters, and managers, and cleared out spawning bugs

--- Assets/00_DodgeBall/N_Scripts/N_GameManager.cs
+++ Assets/00_DodgeBall/N_Scripts/N_GameManager.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using System;
 using System.Linq;
 using Photon.Realtime;
 
-public enum N_Prefab { Player }
+public enum N_Prefab { PlayerManager,Player }
 
 [Serializable]
 public class LoadablePrefab
 {
-    public N_Prefab type = N_Prefab.Player;
-    public string name = "N_Player";
+    public N_Prefab type = N_Prefab.PlayerManager;
+    public string name = "N_PlayerManager";
     public LoadablePrefab(){ }
     public LoadablePrefab(N_Prefab type, string name)
     {
@@ -23,10 +22,17 @@ public class LoadablePrefab
 
 public class N_GameManager : N_Singleton<N_GameManager>
 {
-    [SerializeField] List<LoadablePrefab> prefabs = new List<LoadablePrefab> { new LoadablePrefab(N_Prefab.Player,"N_Player") };
+    [SerializeField] List<LoadablePrefab> prefabs = new List<LoadablePrefab> { new LoadablePrefab(N_Prefab.Player,"N_PlayerManager") };
 
     List<SpawnPoint> playerSpawnPoints = new List<SpawnPoint>();
-
+    void Reset()
+    {
+        prefabs = new List<LoadablePrefab>();
+        foreach (var e in Enum.GetValues(typeof(N_Prefab)).Cast<N_Prefab>())
+        {
+            prefabs.Add(new LoadablePrefab(e,"N_" + e.ToString()));
+        }
+    }
     void Start()
     {
         if(!PhotonNetwork.IsConnected)
@@ -34,17 +40,18 @@ public class N_GameManager : N_Singleton<N_GameManager>
             Debug.LogWarning("Must be connected, for Networked game manager to work");
             return;
         }
+
         print(PhotonNetwork.NickName+ " N_GameManager() :: in room " + PhotonNetwork.CurrentRoom.Name);
         playerSpawnPoints = FindObjectsOfType<SpawnPoint>().ToList();
         int i = UnityEngine.Random.Range(0, playerSpawnPoints.Count);
         SpawnPoint s = playerSpawnPoints[i];
-        MakeObj(N_Prefab.Player, s.position, s.rotation);
+        MakeObj(N_Prefab.PlayerManager, s.position, s.rotation);
     }
 
-    public static GameObject MakeObj(N_Prefab prefab,Vector3 pos,Quaternion rot)
+    public static GameObject MakeObj(N_Prefab prefab, Vector3 pos, Quaternion rot, byte group = 0, object[] data = null)
     {
         LoadablePrefab p = instance.prefabs.Single(f => f.type == prefab);
-        GameObject o = PhotonNetwork.Instantiate(p.name, pos, rot);
+        GameObject o = PhotonNetwork.Instantiate(p.name, pos, rot, group, data);
         return o;
     }
 
