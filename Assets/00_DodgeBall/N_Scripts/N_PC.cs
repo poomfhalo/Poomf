9711a5f1ee33790bf380ab9ce5e607f589b51874
Motasem Zakarneh <mutrz99@gmail.com>
2020-06-02 17:17:35 +0300

    Created, Input, Handler

---
+++ Assets/00_DodgeBall/N_Scripts/N_PC.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+[RequireComponent(typeof(PhotonView))]
+[RequireComponent(typeof(PC))]
+public class N_PC : MonoBehaviour
+{
+    PhotonView photonView = null;
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+        if(photonView && !photonView.IsMine)
+        {
+            photonView.enabled = false;
+        }
+    }
+}
