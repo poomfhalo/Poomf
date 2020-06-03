3cb5de8dfdb17b4be7e0207d80ab7f979f38838e
Motasem Zakarneh <mutrz99@gmail.com>
2020-06-03 14:34:52 +0300

    N_PC fixed, initialization

--- Assets/00_DodgeBall/N_Scripts/N_PC.cs
+++ Assets/00_DodgeBall/N_Scripts/N_PC.cs
@@ -6,17 +6,14 @@ public class N_PC : MonoBehaviour
 {
     public Player controller = null;
     [SerializeField] int actorID = -1;
-    void Start()
-    {
-        if(!controller.IsLocal)
-        {
-            GetComponent<PC>().enabled = false;
-        }
-    }
-
+ 
     public void Initialize(Player controller)
     {
         actorID = controller.ActorNumber;
         this.controller = controller;
+        if (!controller.IsLocal)
+        {
+            GetComponent<PC>().enabled = false;
+        }
     }
 }
