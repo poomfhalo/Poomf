8218aa21610b177b69addd35fade9db976a7a5b9
Motasem Zakarneh <mutrz99@gmail.com>
2020-06-10 17:57:53 +0300

    Created RPC Tester

---
+++ Assets/00_DodgeBall/N_Scripts/RPCTester.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.InputSystem;
+[RequireComponent(typeof(PhotonView))]
+public class RPCTester : MonoBehaviour
+{
+    [SerializeField] InputAction a = null;
+    PhotonView pv = null;
+    void Start()
+    {
+        pv = GetComponent<PhotonView>();
+        a.started += OnStarted;
+    }
+
+    private void OnStarted(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Sending Command ");
+        pv.RPC("RecievingCommand", RpcTarget.All);
+    }
+    [PunRPC]
+    void RecievingCommand()
+    {
+        Debug.Log("Have Recieved Command");
+    }
+    void Update()
+    {
+
+    }
+}
