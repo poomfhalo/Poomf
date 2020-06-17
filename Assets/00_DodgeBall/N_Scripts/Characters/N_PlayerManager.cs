94a8f86f3225b40838f5fcc4cea196912cfedad4
Motasem Zakarneh <mutrz99@gmail.com>
2020-06-17 20:14:24 +0300

    Removed camera code

--- Assets/00_DodgeBall/N_Scripts/Characters/N_PlayerManager.cs
+++ Assets/00_DodgeBall/N_Scripts/Characters/N_PlayerManager.cs
@@ -43,14 +43,13 @@ public class N_PlayerManager : MonoBehaviourPunCallbacks
         {
             pc = N_Extentions.GetCharacter(GetComponent<PhotonView>().Controller.ActorNumber).gameObject;
             team = TeamsManager.GetTeam(pc.GetComponent<DodgeballCharacter>());
-            //pc.GetComponent<DodgeballCharacter>().SetTeam(team.teamTag);
         }
-        if (team == null)
-            return;
+        //if (team == null)
+        //    return;
 
-        Camera cam = Camera.main;
-        var p = FindObjectsOfType<TaggedSpawnPoint>().ToList().Find(s => s.HasTag("MainCamera") && s.BelongsTo(team.teamTag));
-        cam.transform.position = p.position;
-        cam.transform.rotation = p.rotation;
+        //Camera cam = Camera.main;
+        //var p = FindObjectsOfType<TaggedSpawnPoint>().ToList().Find(s => s.HasTag("MainCamera") && s.BelongsTo(team.teamTag));
+        //cam.transform.position = p.position;
+        //cam.transform.rotation = p.rotation;
     }
 }
