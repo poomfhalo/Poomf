fd63083ed4b4cd108e02fc85ac7b42d33415b7ce
korocat <ziadashraf874@gmail.com>
2020-06-23 04:11:59 +0200

    Some progress with the Settings UI, created a general script for any "slider controlled" setting

---
+++ Assets/Scripts/UI/Menu/Settings/SliderControlledSetting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Settings that are controlled with sliders, like volume
+public class SliderControlledSetting : MonoBehaviour
+{
+    // The slider thats's used to control this setting
+    [SerializeField] private Slider sliderComponent;
+    // The text value corresponding to this setting (could be null)
+    [SerializeField] private Text textValue;
+    // Used when setting the text value, multiplies the text value by a multiplier to display the desired range
+    [SerializeField] private float multiplier;
+
+    #region Setters/Getters
+    public Slider SliderComponent { get { return sliderComponent; } }
+    public Text TextValue { get { return textValue; } }
+    public float Multiplier { get { return multiplier; } set { multiplier = value; } }
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
