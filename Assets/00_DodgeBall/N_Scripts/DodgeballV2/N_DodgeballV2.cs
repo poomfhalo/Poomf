a4c64dbe05895d8d06aaae195c6cab1f9e974218
Motasem Zakarneh <mutrz99@gmail.com>
2020-06-27 19:20:18 +0300

    Blocked Reflection Non Owners

--- Assets/00_DodgeBall/N_Scripts/DodgeballV2/N_DodgeballV2.cs
+++ Assets/00_DodgeBall/N_Scripts/DodgeballV2/N_DodgeballV2.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Photon.Pun;
+
+//We Will Testout 2 Methods:
+//1.
+//Keep the Syncer, run appropriately, at the correct times, be disabled/enabled.
+//Have The Authority, being Transfered, to the correct character.
+//Actions only apply on the Appropriate "Owner"
+//2.
+//Do Commands Syncing, ball is local unless on ground, and does snap.
+//Only core Commands get called
+//we have a list of commands, that are synced, whatever gets called on local machine, gets removed from synced list.
+//every period of time, we deal with the ball appropriately, depending on the remaining, highest order most recent command
 
 public class N_DodgeballV2 : MonoBehaviour, IPunObservable
 {
-    [SerializeField] int commandsSnapCount = 3;
-    [SerializeField] List<DodgeballCommand> sentCommands = new List<DodgeballCommand>();
+    //[SerializeField] int commandsSnapCount = 3;
+    //[SerializeField] List<DodgeballCommand> sentCommands = new List<DodgeballCommand>();
 
     PhotonView pv = null;
     Dodgeball ball = null;
