93237d75c9eeb47bc991be0e6c52faecbc69352e
Motasem Zakarneh <mutrz99@gmail.com>
2020-06-30 15:19:36 +0300

    Updated N_SshortStarter to now, load Menu scene, instead of MP Menu

--- Assets/00_DodgeBall/N_Scripts/Managers/GameManagers/N_ShortStarter.cs
+++ Assets/00_DodgeBall/N_Scripts/Managers/GameManagers/N_ShortStarter.cs
@@ -73,7 +73,7 @@ public class N_ShortStarter : MonoBehaviourPunCallbacks
             if (isTeamAEmpty || isTeamBEmpty)
             {
                 Log.Warning("Game Is over, one of the teams is empty");
-                PhotonNetwork.LoadLevel("MP Menu");
+                PhotonNetwork.LoadLevel("Menu");
                 PhotonNetwork.LeaveRoom();
             }
         });
