87480bfb56eaf1cfcffc6f4f58b828b3e1c72d7a
Motasem Zakarneh <mutrz99@gmail.com>
2020-07-03 20:05:31 +0300

    MP Room Massive Progress

---
+++ Assets/00_DodgeBall/N_Scripts/UI/N_Room.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class N_Room : MonoBehaviour
+{
+    [Header("Constants")]
+    [SerializeField] Image levelBGImage = null;
+    [SerializeField] TextMeshProUGUI timerText = null;
+    [Header("Variables")]
+    [Tooltip("In Seconds")]
+    [SerializeField] float levelChangeTimer = 30;
+
+    VoteSlot[] delegators = null;
+    float levelChangeCounter = 0;
+    bool isTimerRunning = false;
+    void Awake()
+    {
+        delegators = GetComponentsInChildren<VoteSlot>();
+        foreach (var d in delegators)
+        {
+            d.E_OnPointerEntered += OnEntered;
+        }
+        levelChangeCounter = levelChangeTimer;
+        isTimerRunning = true;
+    }
+    void Update()
+    {
+        if (!isTimerRunning)
+            return;
+        levelChangeCounter = levelChangeCounter - Time.deltaTime;
+        int minutes = Mathf.FloorToInt(levelChangeCounter) / 60;
+        int seconds = Mathf.FloorToInt(levelChangeCounter) % 60;
+        if(minutes<10)
+        {
+            timerText.text = "0" + minutes + ":" + seconds;
+        }
+        else
+        {
+            timerText.text = minutes + ":" + seconds;
+        }
+
+        if (levelChangeCounter<=0)
+        {
+            isTimerRunning = false;
+            SceneManager.LoadScene(GetComponent<N_VotesBox>().GetWinner());
+        }
+    }
+
+    private void OnEntered(VoteSlot s)
+    {
+        levelBGImage.sprite = s.CoverSprite;
+    }
+}
---
+++ Assets/00_DodgeBall/N_Scripts/UI/N_VotesBox.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Photon.Pun;
+
+public class N_VotesBox : MonoBehaviour
+{
+    [Header("Read Only")]
+    [SerializeField] VoteSlot activeSlot = null;
+    VoteSlot[] slots = null;
+    void Start()
+    {
+        slots = GetComponentsInChildren<VoteSlot>();
+        foreach (var s in slots)
+        {
+            s.E_OnSelected += OnSelected;
+        }
+        foreach (var s in slots)
+        {
+            s.DeSelect();
+        }
+        if (activeSlot != null)
+            activeSlot.OnPointerClick(null);
+    }
+
+    private void OnSelected(VoteSlot s)
+    {
+        activeSlot = s;
+        foreach (var slot in slots)
+        {
+            if (slot == s)
+                continue;
+            slot.DeSelect();
+        }
+    }
+
+    public string GetWinner()
+    {
+        if(activeSlot)
+            return activeSlot.SceneName;
+
+        Log.Warning("Could Not, Local Winner, sending Stadium");
+        return "SP Game";
+    }
+}
