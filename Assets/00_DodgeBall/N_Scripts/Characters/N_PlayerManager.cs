c58bd62b4412e7bd0448d8a7ae4fab149433e15f
Motasem Zakarneh <mutrz99@gmail.com>
2020-07-09 17:50:41 +0300

    Updated Camera spawn positions, to not conflict with cutscenes

--- Assets/00_DodgeBall/N_Scripts/Characters/N_PlayerManager.cs
+++ Assets/00_DodgeBall/N_Scripts/Characters/N_PlayerManager.cs
@@ -51,9 +51,7 @@ public class N_PlayerManager : MonoBehaviourPunCallbacks
             return;
         }
 
-        Camera cam = Camera.main;
         var p = FindObjectsOfType<TaggedSpawnPoint>().ToList().Find(s => s.HasTag("MainCamera") && s.BelongsTo(team.teamTag));
-        cam.transform.position = p.position;
-        cam.transform.rotation = p.rotation;
+        p.GetComponent<Cinemachine.CinemachineVirtualCamera>().Priority = 15;
     }
 }
