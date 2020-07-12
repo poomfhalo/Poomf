e90b60df23af230bc9d7cb34be364950b2498347
Gurin <gurinjaw@gmail.com>
2020-07-13 01:25:44 +0200

    Dirty Menu scene fix : Content Size Fitter Enabler

---
+++ Assets/Scripts/UI/Utilities/ContentSizeFitterEnabler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Poomf.UI
+{
+    public class ContentSizeFitterEnabler : MonoBehaviour
+    {
+        private void Start()
+        {
+            ContentSizeFitter contentSizeFitter = GetComponent<ContentSizeFitter>();
+
+            if (null != contentSizeFitter)
+                contentSizeFitter.enabled = true;
+        }
+    }
+}
