4e496e6e5edd607cce625d32804ce6d5d96639a7
Gurin <gurinjaw@gmail.com>
2020-07-22 01:48:12 +0200

    Currency Manager. Data Debugger.

---
+++ Assets/Scripts/Data/Debugging/DataDebugger.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Poomf.Data
+{
+    public class DataDebugger : MonoBehaviour
+    {
+#if UNITY_EDITOR
+
+        #region UNITY
+        // Start is called before the first frame update
+        void Start()
+        {
+            initialize();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            manageInput();
+        }
+        #endregion
+
+        #region PRIVATE
+
+        void initialize()
+        {
+            debugAllCurrencies();
+        }
+
+        void manageInput()
+        {
+            if (Keyboard.current.cKey.wasPressedThisFrame)
+            {
+                addCoins(5);
+            }
+            else if (Keyboard.current.gKey.wasPressedThisFrame)
+            {
+                addGems(5);
+            }
+        }
+
+        void addCoins(int i_coinsToAdd)
+        {
+            CurrencyManager.AddCoins(i_coinsToAdd);
+            debugCoins();
+        }
+
+        void addGems(int i_gemsToAdd)
+        {
+            CurrencyManager.AddGems(i_gemsToAdd);
+            debugGems();
+        }
+
+        void debugAllCurrencies()
+        {
+            debugCoins();
+            debugGems();
+        }
+
+        void debugCoins()
+        {
+            Debug.LogWarning("Coins value: " + CurrencyManager.Coins);
+        }
+
+        void debugGems()
+        {
+            Debug.LogWarning("Coins value: " + CurrencyManager.Gems);
+        }
+
+        #endregion
+
+#endif
+    }
+}
---
+++ Assets/Scripts/Data/SaveData/CurrencyManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Poomf.Data
+{
+    public class CurrencyManager : MonoBehaviour
+    {
+        private const string coinsKey = "currency_coins";
+        private const string gemsKey = "currency_gems";
+        private const int defaultCoinsValue = 0;
+        private const int defaultGemsValue = 0;
+
+        #region PUBLIC API
+
+        public static void AddCoins(int i_coinsToAdd)
+        {
+            addCoins(i_coinsToAdd);
+        }
+
+        public static void AddGems(int i_gemsToAdd)
+        {
+            addGems(i_gemsToAdd);
+        }
+
+        public static int Coins { get { return SaveManager.GetData(coinsKey, defaultCoinsValue); } }
+        public static int Gems { get { return SaveManager.GetData(gemsKey, defaultGemsValue); } }
+
+        #endregion
+
+        #region PRIVATE
+
+        static void addCoins(int i_coinsToAdd)
+        {
+            addCurrency(coinsKey, defaultCoinsValue, i_coinsToAdd);
+        }
+
+        static void addGems(int i_gemsToAdd)
+        {
+            addCurrency(gemsKey, defaultGemsValue, i_gemsToAdd);
+        }
+
+        static void addCurrency(string i_currencyKey, int i_defaultValue, int i_coinsToAdd)
+        {
+            int currentCurrencyValue = SaveManager.GetData(i_currencyKey, i_defaultValue) + i_coinsToAdd;
+            SaveManager.SaveData(i_currencyKey, currentCurrencyValue);
+        }
+
+        #endregion
+    }
+}
